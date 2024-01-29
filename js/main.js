1c5369ecb485f02cccf8685546c4008b947a1aeb
MinaMalaka <153759949+MinaMalaka@users.noreply.github.com>
2024-01-29 09:13:01 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -242,6 +242,7 @@ async function mealDetail(id) {
 // display fn
 function areaMealDetails(obj) {
     close()
+    searchContent.innerHTML = ''
     $(".loading-in").fadeIn()
     let cartona = ''
     for (let i = 1; i <= 20; i++) {
